120f0dd19f734d0906c04cc9629058e01a53d18a
shahabbasi <34238824+shahabbasi@users.noreply.github.com>
2018-04-25 21:38:40 +0430

    Document model has been created

---
+++ server/model/Document.js
@@ -0,0 +1,36 @@
+const db = require('./db');
+
+const documentSchema = db.Schema({
+    name : {
+        required : true,
+        type : String
+    },
+    text : {
+        required : true,
+        type : String
+    },
+    creator : {
+        required : true,
+        type : db.Schema.Types.ObjectId,
+        ref : "User"
+    },
+    category : {
+        required : true,
+        type : db.Schema.Types.ObjectId ,
+        ref : "Category"
+    },
+    createdAt : {
+        required : true,
+        type : Date,
+        default : Date.now()
+    },
+    modifiedAt : {
+        required : true ,
+        type : Date ,
+        default : Date.now()
+    }
+});
+
+const Document = db.model('Document', documentSchema);
+
+module.exports = Document;
