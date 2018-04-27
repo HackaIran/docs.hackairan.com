e25fcfae15e52857e4664dc9c038a8df0415d420
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-04-28 03:55:00 +0430

    Logo added to page

--- client/bundles/home/index.js
+++ client/bundles/home/index.js
@@ -1,9 +1,23 @@
-const searchBox = $('.search-documents');
-searchBox.onchange = searchBox.onkeyup = () => {
-    const query = searchBox.value.toLowerCase();
-    const allListItems = $$('body > div.column.titles > ul > li');
-    for (let item of allListItems) item.classList.remove('hidden');
-    for (let item of allListItems) {
-        if (!item.innerText.toLowerCase().includes(query)) item.classList.add('hidden');
+class App {
+
+    constructor () {
+        this.initializeSearchBox();
     }
+
+    initializeSearchBox () {
+        this.searchBox = $('.search-documents');
+        this.searchBox.onchange = this.searchBox.onkeyup = this.doSearch.bind(this);
+    }
+
+    doSearch () {
+        const query = this.searchBox.value.toLowerCase();
+        const allListItems = $$('body > div.column.titles > ul > li');
+        for (let item of allListItems) item.classList.remove('hidden');
+        for (let item of allListItems) {
+            if (!item.innerText.toLowerCase().includes(query)) item.classList.add('hidden');
+        }
+    }
+
 }
+
+const app = new App
