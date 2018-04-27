b33e5e20c1db25b22d3b80567bb38dbf0acae7e0
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-04-28 03:29:05 +0430

    more data for testing

--- server/controller/AppController.js
+++ server/controller/AppController.js
@@ -9,10 +9,32 @@ appController.index = function (req, res) {
     // loading all documents
     const documents = [
         {
+            id: 'open-source',
             title: 'Open-Source Guideline',
             creator: 'Mohammad H. Shahin',
             date: '2018-06-29',
             abstract: 'short of description of this guideline is something like this. as you can see sometimes its longer than what you think!'
+        },
+        {
+            id: 'gpl3',
+            title: 'Getting deep in GPL-3',
+            creator: 'Mohammad H. Shahin',
+            date: '2018-07-22',
+            abstract: 'GPL-3 is a famous license, In this article we will asses it deeply!'
+        },
+        {
+            id: 'how-to-work-with-nerdpitch',
+            title: 'How to work with NerdPitch',
+            creator: 'Pouya MozaffarMagham',
+            date: '2018-03-11',
+            abstract: 'NerdPitch is the most beloved tool for sharing knowledge and ideas. Let\'s see how it excatly works!'
+        },
+        {
+            id: 'why-we-held-hackademy',
+            title: 'Why we held Hackademy behind the certain?',
+            creator: 'Alireza Sheikholmolouki',
+            date: '2019-01-01',
+            abstract: 'After one year of holding Hackademy, there are some secrets to leak about why we excatly held hackademy.'
         }
     ]
 
